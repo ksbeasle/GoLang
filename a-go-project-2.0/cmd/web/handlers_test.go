34e5b87ad30daa66176a2e1dff20157325429ced
Kahlil Beasley <ksbeasle@asu.edu>
2020-05-27 14:11:02 -0700

    handlers_test.go lines 45/46

--- a-go-project-2.0/cmd/web/handlers_test.go
+++ a-go-project-2.0/cmd/web/handlers_test.go
@@ -19,30 +19,31 @@ func TestGetGame(t *testing.T) {
 	testCases := []struct {
 		name     string
 		url      string
-		wantBody []byte
 		wantCode int
+		wantBody []byte
 	}{
-		{"Valid Request", "/game/1", []byte("Halo 3"), http.StatusOK},
-		{"Negative ID", "/game/-1", nil, http.StatusNotFound},
-		{"ID doesn't Exists", "/game/0", nil, http.StatusNotFound},
-		{"ID is a String", "/game/str", nil, http.StatusNotFound},
-		{"ID is a decimal", "/game/1.0", nil, http.StatusNotFound},
-		{"ID is empty", "/game/", nil, http.StatusNotFound},
-		{"Trailing slash", "/game/1/", nil, http.StatusNotFound},
+		{"Valid Request", "/game/1", http.StatusOK, []byte("Halo 3")},
+		{"Negative ID", "/game/-1", http.StatusNotFound, nil},
+		{"ID doesn't Exists", "/game/0", http.StatusNotFound, nil},
+		{"ID is a String", "/game/str", http.StatusNotFound, nil},
+		{"ID is a decimal", "/game/1.0", http.StatusNotFound, nil},
+		{"ID is empty", "/game/", http.StatusNotFound, nil},
+		{"Trailing slash", "/game/1/", http.StatusNotFound, nil},
 	}
 
 	//loop through testCases
 	for _, tc := range testCases {
 		code, body := server.get(t, tc.url)
-
+		fmt.Println("code", code)
+		fmt.Println(tc.wantCode)
 		//check if the status code matches
 		if code != tc.wantCode {
-			fmt.Println("code", code)
-			fmt.Println(tc.wantCode)
-			t.Errorf("\nGot: %q\nWant: %q", code, http.StatusNotFound)
+
+			t.Errorf("\nGot: %q\nWant: %d", code, tc.wantCode)
 		}
 
-		if !bytes.Contains(tc.wantBody, body) {
+		//HOW DOES !bytes.Contains(tc.wantBody, body) affect the code test case????
+		if !bytes.Contains(body, tc.wantBody) {
 			t.Errorf("\nGot: %q\nWant: %q", body, tc.wantBody)
 		}
 	}
