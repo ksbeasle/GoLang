791b007223fbd7efb0235c7177db682b1be6570f
Kahlil Beasley <ksbeasle@asu.edu>
2020-06-18 22:33:57 -0700

    Added Insert test case

--- a-go-project-2.0/pkg/models/mysql/games_test.go
+++ a-go-project-2.0/pkg/models/mysql/games_test.go
@@ -62,3 +62,50 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func TestInsert(t *testing.T) {
+
+	tests := []struct {
+		Name         string
+		GameID       int
+		InsertedGame *models.Game
+		wantError    error
+		wantId       int
+	}{
+		{
+			Name:   "Valid Insert",
+			GameID: 1,
+			InsertedGame: &models.Game{
+				ID:          1,
+				Title:       "Test",
+				Genre:       "Test",
+				Rating:      10,
+				Platform:    "Test",
+				ReleaseDate: "January 1, 2000",
+			},
+			wantError: nil,
+			wantId:    2,
+		},
+	}
+
+	for _, testcase := range tests {
+		t.Run(testcase.Name, func(t *testing.T) {
+			db, teardownDB := createTestDB(t)
+
+			defer teardownDB()
+
+			v := VGModel{db}
+			//title string, genre string, rating int, platform string, releaseDate string
+			id, err := v.Insert(testcase.InsertedGame.Title, testcase.InsertedGame.Genre, testcase.InsertedGame.Rating, testcase.InsertedGame.Platform, testcase.InsertedGame.ReleaseDate)
+
+			if id != testcase.wantId {
+				t.Errorf("\nGot: %d\nWant: %v", id, testcase.wantId)
+			}
+
+			if err != testcase.wantError {
+				t.Errorf("\nGot: %s\nWant: %v", err, testcase.wantError)
+			}
+		})
+	}
+
+}
